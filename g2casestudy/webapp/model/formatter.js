fe9b4c585e984ebe4a00ca4c89e0e15dd79ffa71
Bien Gilzon Lara <bien.gilzon.c.lara@accenture.com>
2026-05-23 16:31:04 +0000

    status colors

--- g2casestudy/webapp/model/formatter.js
+++ g2casestudy/webapp/model/formatter.js
@@ -5,14 +5,32 @@ sap.ui.define([
   "use strict";
 
   return {
-
+    // format to "DD MM YYYY"
     formatDate: function (sDate) {
       if (sDate) {
         var oDate = new Date(sDate);
-        // format to "DD MM YYYY"
         return oDate.toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' });
       }
       return sDate;
+    },
+
+    // return object status state based on status value
+    getStatusState: function (status) {
+      switch (status) {
+        case "Created":
+          return "None";
+          break;
+        case "Released":
+          return "Warning";
+          break;
+        case "Partially Completed":
+          return "Information";
+          break;
+        case "Delivered":
+          return "Success";
+          break;
+      }
+      
     }
 
   };
