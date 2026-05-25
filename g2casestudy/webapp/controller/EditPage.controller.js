548893710ea7bd4f72178e4dc985d18e2bb7fd95
Jinky Lopez <jinky.a.lopez@accenture.com>
2026-05-25 12:33:36 +0000

    added onCancel function

--- g2casestudy/webapp/controller/EditPage.controller.js
+++ g2casestudy/webapp/controller/EditPage.controller.js
@@ -23,8 +23,24 @@ sap.ui.define([
         emphasizedAction: MessageBox.Action.YES,
         onClose: function (sAction) {
           if (sAction === MessageBox.Action.YES) {
-            // Logic to save changes goes here
             MessageToast.show("The Order <Order Number> has been updated successfully.");
+          }
+        }
+      });
+    },
+
+    // Show confirmation when Deleting
+
+    // Show confirmation when Canceling
+    onCancel: function () {
+      MessageBox.confirm("Are you sure you want to cancel the changes done in the page?", {
+        title: "Confirm Cancel",
+        actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+        emphasizedAction: MessageBox.Action.YES,
+        onClose: function (sAction) {
+          if (sAction === MessageBox.Action.YES) {
+            // Navigate back to Detail page
+            window.history.go(-1);
           }
         }
       });
