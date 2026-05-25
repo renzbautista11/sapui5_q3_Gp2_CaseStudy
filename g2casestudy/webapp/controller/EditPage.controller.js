13e501f7e62bf27e9547ca4709f5fb4cebd3fdb6
Jinky Lopez <jinky.a.lopez@accenture.com>
2026-05-25 10:18:57 +0000

    initil Edit Page controller

---
+++ g2casestudy/webapp/controller/EditPage.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], function (Controller) {
+  "use strict";
+
+  return Controller.extend("g2casestudy.controller.EditPage", {
+    onInit: function () {
+      // Initialization code for EditPage controller
+    }
+  });
+});
