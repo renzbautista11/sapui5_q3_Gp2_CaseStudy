e994e8dba86096818957739ae8c1f83ca0f9dc07
Jinky Lopez <jinky.a.lopez@accenture.com>
2026-05-25 10:28:41 +0000

    onSave function added

--- g2casestudy/webapp/controller/EditPage.controller.js
+++ g2casestudy/webapp/controller/EditPage.controller.js
@@ -1,11 +1,28 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], function (Controller) {
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageBox",
+  "sap/m/MessageToast"
+], function (Controller, MessageBox, MessageToast) {
   "use strict";
 
   return Controller.extend("g2casestudy.controller.EditPage", {
     onInit: function () {
       // Initialization code for EditPage controller
+    },
+
+    // Show confirmation when Saving
+    onSave: function () {
+      MessageBox.confirm("Are you sure you want to Save these changes?", {
+        title: "Confirm Save",
+        actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+        emphasizedAction: MessageBox.Action.YES,
+        onClose: function (sAction) {
+          if (sAction === MessageBox.Action.YES) {
+            // Logic to save changes goes here
+            MessageToast.show("The Order <Order Number> has been updated successfully.");
+          }
+        }
+      });
     }
   });
 });
